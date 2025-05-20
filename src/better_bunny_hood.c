4f72f38602d191dc55fd3fbe3daa72a44ca22ded
LoadingErr <110636305+LoadingErr@users.noreply.github.com>
2025-05-19 20:43:02 -0600

    Update to v1.0.0 and include decomp submodules

--- src/better_bunny_hood.c
+++ src/better_bunny_hood.c
@@ -10,6 +10,9 @@
 #include "controller.h"
 #include "kaleido_manager.h"
 #include "z64item.h"
+#include "z64interface.h"
+#include "gfx_setupdl.h"
+#include "z64camera.h"
 
 typedef enum PauseEquipCButton {
     /* 0 */ PAUSE_EQUIP_C_LEFT,
@@ -65,10 +68,29 @@ s8 sActionHandlerList8[] = {
     /* 11 */ -PLAYER_ACTION_HANDLER_6,
 };
 
+typedef enum {
+    /* 0 */ POSTMAN_MINIGAME_BUNNY_HOOD_OFF,
+    /* 2 */ POSTMAN_MINIGAME_BUNNY_HOOD_ON = 2
+} PostmanMinigameBunnyHoodState;
+
+extern s16 sPostmanBunnyHoodState;
+
 #define SPEED_MODE_CURVED 0.018f
 #define gSPDisplayList(pkt,dl)  gDma1p(pkt, G_DL, dl, 0, G_DL_PUSH)
+#define MASK_GRID_CELL_WIDTH 32
+#define MASK_GRID_CELL_HEIGHT 32
+#define MASK_GRID_QUAD_MARGIN 2
+#define MASK_GRID_QUAD_WIDTH (MASK_GRID_CELL_WIDTH - (2 * MASK_GRID_QUAD_MARGIN))
+#define MASK_GRID_QUAD_HEIGHT (MASK_GRID_CELL_HEIGHT - (2 * MASK_GRID_QUAD_MARGIN))
+#define MASK_GRID_QUAD_TEX_SIZE 32 // both width and height
+#define MASK_GRID_QUAD_ENLARGE_OFFSET 2
+#define MASK_GRID_SELECTED_QUAD_MARGIN (-2)
+#define MASK_GRID_SELECTED_QUAD_WIDTH (MASK_GRID_QUAD_WIDTH - (2 * MASK_GRID_SELECTED_QUAD_MARGIN))
+#define MASK_GRID_SELECTED_QUAD_HEIGHT (MASK_GRID_QUAD_HEIGHT - (2 * MASK_GRID_SELECTED_QUAD_MARGIN))
+#define MASK_GRID_SELECTED_QUAD_TEX_SIZE 32 // both width and height
 
 void Interface_UpdateButtonAlphasByStatus(PlayState* play, s16 risingAlpha);
+void KaleidoScope_SetCursorVtxPos(PauseContext* pauseCtx, u16 vtxIndex, Vtx* vtx);
 s32 Player_GetMovementSpeedAndYaw(Player* this, f32* outSpeedTarget, s16* outYawTarget, f32 speedMode, PlayState* play);
 s32 Player_TryActionHandlerList(PlayState* play, Player* this, s8* actionHandlerList, s32 updateUpperBody);
 s32 func_8083A4A4(Player* this, f32* speedTarget, s16* yawTarget, f32 decelerationRate);
@@ -154,6 +176,77 @@ void DisableHoodIfBunnyModeEnabled(PlayState* play, Input* input) {
     }
 }
 
+RECOMP_HOOK("KaleidoScope_DrawMaskSelect") void DrawEquipSquare(PlayState* play) {
+    static Vtx* persistentMasksVtx;
+
+    if (BunnyHoodMode_IsEnabled) {
+            GraphicsContext* gfxCtx = play->state.gfxCtx;
+            PauseContext* pauseCtx = &play->pauseCtx;
+            s16 i = 0;
+            s16 j = 0;
+            s16 k;
+
+            OPEN_DISPS(gfxCtx);
+
+            persistentMasksVtx = (Vtx*)GRAPH_ALLOC(gfxCtx, (4 * 4) * sizeof(Vtx));
+
+            if (pauseCtx->state == PAUSE_STATE_MAIN) {
+                s16 slot = SLOT_MASK_BUNNY - ITEM_NUM_SLOTS;
+                s16 slotX = slot % MASK_GRID_COLS;
+                s16 slotY = slot / MASK_GRID_COLS;
+                s16 initialX = 0 - (MASK_GRID_COLS * MASK_GRID_CELL_WIDTH) / 2;
+                s16 initialY = (MASK_GRID_ROWS * MASK_GRID_CELL_HEIGHT) / 2 - 6;
+                s16 vtxX = (initialX + (slotX * MASK_GRID_CELL_WIDTH)) + MASK_GRID_QUAD_MARGIN;
+                s16 vtxY = (initialY - (slotY * MASK_GRID_CELL_HEIGHT)) + pauseCtx->offsetY - MASK_GRID_QUAD_MARGIN;
+                persistentMasksVtx[i + 0].v.ob[0] = persistentMasksVtx[i + 2].v.ob[0] =
+                    vtxX + MASK_GRID_SELECTED_QUAD_MARGIN;
+                persistentMasksVtx[i + 1].v.ob[0] = persistentMasksVtx[i + 3].v.ob[0] =
+                    persistentMasksVtx[i + 0].v.ob[0] + MASK_GRID_SELECTED_QUAD_WIDTH;
+                persistentMasksVtx[i + 0].v.ob[1] = persistentMasksVtx[i + 1].v.ob[1] =
+                    vtxY - MASK_GRID_SELECTED_QUAD_MARGIN;
+
+                persistentMasksVtx[i + 2].v.ob[1] = persistentMasksVtx[i + 3].v.ob[1] =
+                    persistentMasksVtx[i + 0].v.ob[1] - MASK_GRID_SELECTED_QUAD_HEIGHT;
+
+                persistentMasksVtx[i + 0].v.ob[2] = persistentMasksVtx[i + 1].v.ob[2] =
+                    persistentMasksVtx[i + 2].v.ob[2] = persistentMasksVtx[i + 3].v.ob[2] = 0;
+
+                persistentMasksVtx[i + 0].v.flag = persistentMasksVtx[i + 1].v.flag = persistentMasksVtx[i + 2].v.flag =
+                    persistentMasksVtx[i + 3].v.flag = 0;
+
+                persistentMasksVtx[i + 0].v.tc[0] = persistentMasksVtx[i + 0].v.tc[1] =
+                    persistentMasksVtx[i + 1].v.tc[1] = persistentMasksVtx[i + 2].v.tc[0] = 0;
+
+                persistentMasksVtx[i + 1].v.tc[0] = persistentMasksVtx[i + 2].v.tc[1] =
+                    persistentMasksVtx[i + 3].v.tc[0] = persistentMasksVtx[i + 3].v.tc[1] =
+                        MASK_GRID_SELECTED_QUAD_TEX_SIZE * (1 << 5);
+
+                persistentMasksVtx[i + 0].v.cn[0] = persistentMasksVtx[i + 1].v.cn[0] =
+                    persistentMasksVtx[i + 2].v.cn[0] = persistentMasksVtx[i + 3].v.cn[0] =
+                        persistentMasksVtx[i + 0].v.cn[1] = persistentMasksVtx[i + 1].v.cn[1] =
+                            persistentMasksVtx[i + 2].v.cn[1] = persistentMasksVtx[i + 3].v.cn[1] =
+                                persistentMasksVtx[i + 0].v.cn[2] = persistentMasksVtx[i + 1].v.cn[2] =
+                                    persistentMasksVtx[i + 2].v.cn[2] = persistentMasksVtx[i + 3].v.cn[2] = 255;
+
+                persistentMasksVtx[i + 0].v.cn[3] = persistentMasksVtx[i + 1].v.cn[3] =
+                    persistentMasksVtx[i + 2].v.cn[3] = persistentMasksVtx[i + 3].v.cn[3] = pauseCtx->alpha;
+        for (i = 0, j = MASK_NUM_SLOTS * 4; i < 3; i++, j += 4) {
+            if (GET_CUR_FORM_BTN_ITEM(i + 1) != ITEM_NONE) {
+                if (GET_CUR_FORM_BTN_SLOT(i + 1) < ITEM_NUM_SLOTS) {
+                    gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 100, 200, 255, 255);
+                    gSPVertex(POLY_OPA_DISP++, (uintptr_t)persistentMasksVtx, 4, 0);
+                    POLY_OPA_DISP = Gfx_DrawTexQuadIA8(POLY_OPA_DISP, (TexturePtr)gEquippedItemOutlineTex, 32, 32, 0);
+                }
+            }
+        }
+
+        gDPPipeSync(POLY_OPA_DISP++);
+
+        CLOSE_DISPS(play->state.gfxCtx);
+    }
+}
+}
+
 // Equip/unequip Bunny Hood and play equip/cancel sfx on A press
 void PreKaleidoHandler(PlayState* play) {
     PauseContext* pauseCtx = &play->pauseCtx;
@@ -167,28 +260,6 @@ void PreKaleidoHandler(PlayState* play) {
                         if (!BunnyHoodMode_IsEnabled) {
                             BunnyHoodMode_IsEnabled = true;
                             Audio_PlaySfx(NA_SE_SY_DECIDE);
-
-                            // PauseContext* pauseCtx = &play->pauseCtx;
-                            // u16 i;
-                            // u16 j;
-                        
-                            // OPEN_DISPS(play->state.gfxCtx);
-                        
-                            // Gfx_SetupDL42_Opa(play->state.gfxCtx);
-                        
-                            // gDPSetCombineMode(POLY_OPA_DISP++, G_CC_MODULATEIA_PRIM, G_CC_MODULATEIA_PRIM);
-                            // gDPSetPrimColor(POLY_OPA_DISP++, 0, 0, 255, 255, 255, pauseCtx->alpha);
-                            // for (i = 0, j = ITEM_NUM_SLOTS * 4; i < 3; i++, j += 4) {
-                            //     if (GET_CUR_FORM_BTN_ITEM(i + 1) != ITEM_NONE) {
-                            //         if (GET_CUR_FORM_BTN_SLOT(i + 1) < ITEM_NUM_SLOTS) {
-                            //             gSPVertex(POLY_OPA_DISP++, &pauseCtx->maskVtx[104], 4, 0);
-                            //             POLY_OPA_DISP = Gfx_DrawTexQuadIA8(POLY_OPA_DISP, gEquippedItemOutlineTex, 32, 32, 0);
-                            //         }
-                            //     }
-                            // }
-
-                            // CLOSE_DISPS(play->state.gfxCtx);
-
                             CONTROLLER1(&play->state)->press.button &= ~BTN_A;
                         } else {
                             BunnyHoodMode_IsEnabled = false;
@@ -250,10 +321,17 @@ RECOMP_PATCH void Player_Action_13(Player* this, PlayState* play) {
 RECOMP_HOOK("Player_PostLimbDrawGameplay")
 void On_Player_PostLimbDrawGameplay(PlayState* play, s32 limbIndex, Gfx** dList1, Gfx** dList2, Vec3s* rot, Actor* actor) {
     Player* player = GET_PLAYER(play);
-    if (!(player->stateFlags1 & PLAYER_STATE1_100000) && 
-    (INV_CONTENT(ITEM_MASK_BUNNY) == ITEM_MASK_BUNNY) && 
+    if (player->stateFlags1 & PLAYER_STATE1_100000) {
+        Vec3f temp;
+        SkinMatrix_Vec3fMtxFMultXYZ(&play->viewProjectionMtxF, &player->actor.focus.pos, &temp);
+        if (temp.z < -4.0f) {
+            return;
+        }
+    }
+
+    if ((INV_CONTENT(ITEM_MASK_BUNNY) == ITEM_MASK_BUNNY) && 
     (BunnyHoodMode_IsEnabled) && 
-    ((player->currentMask == PLAYER_MASK_NONE) || (player->currentMask == PLAYER_MASK_DEKU) || (player->currentMask == PLAYER_MASK_GORON) || (player->currentMask == PLAYER_MASK_ZORA))) {
+    ((player->currentMask == PLAYER_MASK_NONE) || (player->currentMask == PLAYER_MASK_DEKU) || (player->currentMask == PLAYER_MASK_GORON) || (player->currentMask == PLAYER_MASK_ZORA) || (player->currentMask == PLAYER_MASK_FIERCE_DEITY))) {
         if (limbIndex == PLAYER_LIMB_HEAD) {
             OPEN_DISPS(play->state.gfxCtx);
 
@@ -280,4 +358,10 @@ void On_Player_PostLimbDrawGameplay(PlayState* play, s32 limbIndex, Gfx** dList1
 
 RECOMP_HOOK("Player_UpdateCommon") void DisableBunny(Player* this, PlayState* play, Input* input) {
     DisableHoodIfBunnyModeEnabled(play, input);
+}
+
+RECOMP_HOOK_RETURN("Interface_StartPostmanTimer") void on_PostmanTimer(s16 seconds, s16 bunnyHoodState) {
+    if (BunnyHoodMode_IsEnabled) {
+        sPostmanBunnyHoodState = POSTMAN_MINIGAME_BUNNY_HOOD_ON;
+    }
 }
